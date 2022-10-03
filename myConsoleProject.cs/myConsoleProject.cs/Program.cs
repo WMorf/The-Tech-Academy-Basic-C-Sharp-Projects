c3812172fc7fd2c5d8e01845b7ce95629fac859f
Wesley Morford <wesmorford@outlook.com>
2022-10-03 15:20:17 -0700

    math basics

--- myConsoleProject.cs/myConsoleProject.cs/Program.cs
+++ myConsoleProject.cs/myConsoleProject.cs/Program.cs
@@ -7,10 +7,29 @@ namespace myConsoleProject.cs
     {
         static void Main()
         {
-            Console.WriteLine("What is your name?");
-            string name = Console.ReadLine();
-            Console.WriteLine("Hello, " + name + "!");
-            Console.WriteLine("Look at this quote \"");
+            //Console.WriteLine("What is your name?");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("Hello, " + name + "!");
+            //Console.WriteLine("Look at this quote \"");
+            //Console.Read();
+
+            int num1 = 3;
+            int num2 = 6;
+            int sum = num1 + num2;
+            Console.WriteLine(sum);
+
+
+            int diff = num1 - num2;
+            Console.WriteLine(diff);
+
+            int product = num1 * num2;
+            Console.WriteLine(product);
+
+            int quotient = num1 / num2;
+            Console.WriteLine(quotient);
+
+            int remainder = num1 % num2;
+            Console.WriteLine(remainder);
             Console.Read();
         }
     }
