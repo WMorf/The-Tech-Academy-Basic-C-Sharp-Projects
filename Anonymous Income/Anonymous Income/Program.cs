4841f1101f92594efb986bfb613800db7ad15fe4
Wesley Morford <wesmorford@outlook.com>
2022-10-04 18:32:53 -0700

    Added income program

---
+++ Anonymous Income/Anonymous Income/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anonymous_Income
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Anonymous Income Comparison Program \n");
+
+            // Person 1
+            Console.WriteLine("Person1");
+            Console.WriteLine("Input Hourly Rate");
+            string person1hr = Console.ReadLine();
+            Console.WriteLine("Input Hours Worked per Week");
+            string person1wrk = Console.ReadLine();
+
+            //Convert inputs to floats
+            float p1hr = Convert.ToSingle(person1hr);
+            float p1wk = Convert.ToSingle(person1wrk);
+            //Calculate rate * hrs worked * 52 weeks in a year
+            float person1all = p1hr * p1wk * 52;
+            Console.WriteLine("Person 1 makes " + person1all + "\n");
+
+
+            // Person 2
+            Console.WriteLine("Person2");
+            Console.WriteLine("Input Hourly Rate");
+            string person2hr = Console.ReadLine();
+            Console.WriteLine("Input Hours Worked per Week");
+            string person2wrk = Console.ReadLine();
+
+            //Convert inputs to floats
+            float p2hr = Convert.ToSingle(person2hr);
+            float p2wk = Convert.ToSingle(person2wrk);
+            //Calculate rate * hrs worked * 52 weeks in a year
+            float person2all = p2hr * p2wk * 52;
+            Console.WriteLine("Person 2 makes " + person2all + "\n");
+
+
+            Console.WriteLine("Does Person 1 make more than Person 2?");
+            Console.ReadLine();
+            //Check if person 1 makes more than person 2
+            bool makeMore = person1all > person2all;
+            Console.WriteLine(makeMore);
+
+            //end
+            Console.ReadLine();
+            Console.WriteLine("End Program");
+
+            Console.ReadLine();
+            Console.WriteLine("Are you still here?");
+
+            Console.ReadLine();
+            Console.WriteLine("The program is over, you can leave now.");
+
+            Console.ReadLine();
+            Console.WriteLine("Go home.");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Math Basics/Math Basics/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_Basics
+{
+    class Program
+    {
+        static void Main()
+        {
+            //
+            Console.WriteLine("Multiply by 50");
+            string by50 = Console.ReadLine();
+            int b50 = Convert.ToInt32(by50);
+            int product = b50 * 50;
+            Console.WriteLine(product);
+
+            //
+            Console.WriteLine("Add 25");
+            string add25 = Console.ReadLine();
+            int a25 = Convert.ToInt32(add25);
+            int sum = a25 + 25;
+            Console.WriteLine(sum);
+
+            //
+            Console.WriteLine("Divide by 12.5");
+            string divby12half = Console.ReadLine();
+            float div12 = Convert.ToSingle(divby12half);
+            float quotient = div12 / 12.5f;
+            Console.WriteLine(quotient);
+
+            //
+            Console.WriteLine("Greater than 50?");
+            string greater50 = Console.ReadLine();
+            int g50 = Convert.ToInt32(greater50);
+            bool greater = g50 > 50;
+            Console.WriteLine(greater);
+
+            //
+            Console.WriteLine("Divide by 7 and give remainder");
+            string div7remain = Console.ReadLine();
+            float div7 = Convert.ToSingle(div7remain);
+            float remainder = div7 % 7;
+            Console.WriteLine(remainder);
+
+            //
+            Console.Read();
+        }
+    }
+}
