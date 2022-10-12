a8974c0ca5bfb66ecf9efbb36f6f1ce9adb3be6d
Wesley Morford <wesmorford@outlook.com>
2022-10-12 16:20:47 -0700

    Working

---
+++ Step-281 - Method/Step-281 - Method/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step_281___Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThisClass NewClass = new ThisClass();
+            NewClass.TakesTwo(5, 6);
+            NewClass.TakesTwo(int1: 4, int2: 10);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Step-281 - Method/Step-281 - Method/ThisClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step_281___Method
+{
+    class ThisClass
+    {
+        public void TakesTwo(int int1, int int2)
+        {
+            int result = int1 + 5;
+            Console.WriteLine("Second Integer: " + int2);
+        }
+    }
+}
