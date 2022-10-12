c81be7ea7a57e6a5c1da453aee1ac3ebd733dd6e
Wesley Morford <wesmorford@outlook.com>
2022-10-12 12:35:56 -0700

    Added Methods assignment

---
+++ Methods/Methods/MyClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class MyClass
+    {
+        // takes input, adds 5 and returns sum
+        public static int AddFive(int input)
+        {
+            int sum = input + 5;
+            return sum;
+        }
+
+        //takes input, multiplies by 5, returns product
+        public static int TimesFive(int input)
+        {
+            int product = input * 5;
+            return product;
+        }
+
+        //takes input, divides by 5, returns quotient
+        public static int DivideFive(int input)
+        {
+            int q = input / 5;
+            return q;
+        }
+    }
+}
---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Choose a number to do math operations on.");
+
+            //takes input from user
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            //prints returned input to console
+            Console.WriteLine(MyClass.AddFive(input));
+
+            Console.WriteLine(MyClass.TimesFive(input));
+
+            Console.WriteLine(MyClass.DivideFive(input));
+
+            Console.ReadLine();
+        }
+    }
+}
