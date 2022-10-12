e36efd8502ba2f6d9cd25c6fc753fb552ed053b4
Wesley Morford <wesmorford@outlook.com>
2022-10-12 14:46:52 -0700

    finish main method

---
+++ Main Method/Main Method/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate new ThisClass class
+            ThisClass newClass = new ThisClass();
+
+            try
+            {
+                //takes in, math, returns int
+                Console.WriteLine(newClass.NewMethod(8));
+                //takes decimal,math, returns int
+                Console.WriteLine(newClass.NewMethod(6.5m));
+                //takes string, tries to convert to int, math, returns int
+                Console.WriteLine(newClass.NewMethod("5"));
+            }
+            //ensures proper input provided, prints error message if not
+            catch(FormatException)
+            {
+                Console.WriteLine("Incorrect format given.");
+            }
+
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Main Method/Main Method/ThisClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method
+{
+    class ThisClass
+    {
+        //takes in, math, returns int
+        public int NewMethod(int input, int option = 1)
+        {
+            int answer = input * option * 5;
+            return answer;
+        }
+
+        //takes decimal,math, returns int
+        public int NewMethod(decimal input)
+        {
+            int answer = Convert.ToInt32(input) - 2;
+            return answer;
+        }
+
+        //takes string, tries to convert to int, math, returns int
+        public int NewMethod(string input)
+        {
+            int answer = Convert.ToInt32(input) + 2;
+            return answer;
+        }
+    }
+}
