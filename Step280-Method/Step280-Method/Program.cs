f215e4af8a483801518ba58a3ef9d4948d97164d
Wesley Morford <wesmorford@outlook.com>
2022-10-12 15:56:43 -0700

    Working

---
+++ Step280-Method/Step280-Method/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step280_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThisClass NewClass = new ThisClass();
+            Console.WriteLine("Choose numbers to add 5 to \n");
+
+            try
+            {
+                //get input from user
+                Console.WriteLine("Please enter the first number");
+                int input = Convert.ToInt32(Console.ReadLine());
+
+                //get optional input from user
+                Console.WriteLine("Please enter the second number. (NOT REQUIRED)");
+                string strinput = Console.ReadLine();
+
+                //if the second input is not blank, run method with both inputs entered
+                if (strinput != "")
+                {
+                    int optInput = Convert.ToInt32(strinput);
+                    Console.WriteLine(NewClass.TakesTwo(input, optInput));
+                }
+                //if second input empty, run method without second input
+                else
+                {
+                    Console.WriteLine(NewClass.TakesTwo(input));
+                }
+
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Incorrect Format provided");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ Step280-Method/Step280-Method/ThisClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step280_Method
+{
+    class ThisClass
+    {
+        //takes in an int and an optional int, does math, returns answer
+        public int TakesTwo(int input, int optInput = 0)
+        {
+            int answer = input + optInput + 5;
+            return answer;
+        }
+    }
+}
