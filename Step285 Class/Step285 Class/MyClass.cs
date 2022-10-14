98ddc55b881cd7dca9c753da6b2b9260c51e0a58
Wesley Morford <wesmorford@outlook.com>
2022-10-14 14:05:30 -0700

    working

--- Step285 Class/Step285 Class/MyClass.cs
+++ Step285 Class/Step285 Class/MyClass.cs
@@ -16,7 +16,7 @@ namespace Step285_Class
         }
 
         //Step 4 method with output parameters
-        public string SpaceFromZero(int input, out int spaces)
+        public string SpacesBetween(int input, out int spaces)
         {
             spaces = 0 + input;
             string howMany = "Your number is " + spaces + " from zero";
@@ -24,10 +24,10 @@ namespace Step285_Class
         }
 
         //Step 5 Overload method
-        public bool SpaceFromZero(int input)
+        public float SpacesBetween(float input1, float input2)
         {
-            bool positive = 0 < input;
-            return positive;
+            float spaces = input1 - input2;
+            return spaces;
         }
 
         //Step6 Declare a class to be static
--- Step285 Class/Step285 Class/Program.cs
+++ Step285 Class/Step285 Class/Program.cs
@@ -17,26 +17,29 @@ namespace Step285_Class
             Console.WriteLine("Pick a number to split in half!");
             int input = Convert.ToInt32(Console.ReadLine());
             newClass.MyMethod(input);
-            Console.WriteLine(newClass.output);
-            Console.ReadLine();
+            Console.WriteLine(newClass.output + "\n");
 
             //Call Step 4
+            Console.WriteLine("Spaces from zero");
             Console.WriteLine("Pick a number, again");
             int number = Convert.ToInt32(Console.ReadLine());
-            string overload = newClass.SpaceFromZero(number);
-            Console.WriteLine();
-            Console.ReadLine();
+            string overload = newClass.SpacesBetween(number, out int spaces);
+            Console.WriteLine(overload + "\n");
 
             //Call Step 5
-            Console.WriteLine("Pick another number");
-            int number2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(number2 + " is a positive number:" + newClass.SpaceFromZero(number2));
-
-
+            Console.WriteLine("Spaces between 2 floats");
+            Console.WriteLine("Pick the first float");
+            float float1 = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Pick the second float");
+            float float2 = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine(" Your numbers have a difference of " + newClass.SpacesBetween(float1, float2) + "\n");
 
             //Call Step 6
+            Console.WriteLine("Add 257 to a number");
             Console.WriteLine("Pick a number one more time");
-            int number3 = Convert.ToInt32(Console.ReadLine());
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(number2 + " + 257 = " + MyClass.Add257(number2));
+            Console.ReadLine();
 
         }
     }
