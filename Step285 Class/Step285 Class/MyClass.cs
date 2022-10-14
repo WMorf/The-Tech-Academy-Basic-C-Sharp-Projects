11da794f2f78ce8eceaa487ad82810f18f7a16ed
Wesley Morford <wesmorford@outlook.com>
2022-10-13 20:38:13 -0700

    working

---
+++ Step285 Class/Step285 Class/MyClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step285_Class
+{
+    class MyClass
+    {
+        //Step 1 Create a void method 
+        public int output { get; set; }
+        public void MyMethod(int input)
+        {
+            output = input / 2;
+        }
+
+        //Step 4 method with output parameters
+        public string SpaceFromZero(int input, out int spaces)
+        {
+            spaces = 0 + input;
+            string howMany = "Your number is " + spaces + " from zero";
+            return howMany;
+        }
+
+        //Step 5 Overload method
+        public bool SpaceFromZero(int input)
+        {
+            bool positive = 0 < input;
+            return positive;
+        }
+
+        //Step6 Declare a class to be static
+
+        public static int Add257(int input)
+        {
+            int  sum = input + 257;
+            return sum;
+        }
+
+
+    }
+}
---
+++ Step285 Class/Step285 Class/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step285_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Step 2
+            MyClass newClass = new MyClass();
+
+            //Step 3 use method on user input and print result to screen
+            Console.WriteLine("Pick a number to split in half!");
+            int input = Convert.ToInt32(Console.ReadLine());
+            newClass.MyMethod(input);
+            Console.WriteLine(newClass.output);
+            Console.ReadLine();
+
+            //Call Step 4
+            Console.WriteLine("Pick a number, again");
+            int number = Convert.ToInt32(Console.ReadLine());
+            string overload = newClass.SpaceFromZero(number);
+            Console.WriteLine();
+            Console.ReadLine();
+
+            //Call Step 5
+            Console.WriteLine("Pick another number");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(number2 + " is a positive number:" + newClass.SpaceFromZero(number2));
+
+
+
+            //Call Step 6
+            Console.WriteLine("Pick a number one more time");
+            int number3 = Convert.ToInt32(Console.ReadLine());
+
+        }
+    }
+}
