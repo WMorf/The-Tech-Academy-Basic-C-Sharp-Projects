d64802b5cd627abc1d7c1ee6ee8fdbbd15bdbd95
Wesley Morford <wesmorford@outlook.com>
2022-10-14 19:08:34 -0700

    add Method and Objects Application

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -10,6 +10,12 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
+            //TwentyOneGame game = new TwentyOneGame();
+            //game.Players = new List<string>() { "Rick", "Benny", "Evee", "Johnathan" };
+            //game.ListPlayers();
+            //game.Play();
+            //Console.ReadLine();
+
             Deck deck = new Deck();
             deck.Shuffle(3);
 
@@ -20,15 +26,5 @@ namespace TwentyOne
             Console.WriteLine(deck.Cards.Count);
             Console.ReadLine();
         }
-
-
-        //public static Deck Shuffle(Deck deck, int times)
-        //{
-        //    for (int i = 0; i < times; i++)
-        //    {
-        //        deck = Shuffle(deck);
-        //    }
-        //    return deck;
-        //}
     }
 }
