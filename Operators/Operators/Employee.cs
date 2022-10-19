649eda632f64e98ec5d655fa8c6da0de88663e67
Wesley Morford <wesmorford@outlook.com>
2022-10-18 17:55:21 -0700

    add Operator

---
+++ Operators/Operators/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Employee : Person, IQuittable
+    {
+        //Step 1 Create an Employee class with Id, FirstName and LastName properties. 
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //Step 2 overload the “==” operator and it's pair !=
+        public static bool operator== (Employee employee1, Employee employee2)
+        {
+            return employee1.Id == employee2.Id;
+        }
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            return employee1.Id != employee2.Id;
+        }
+
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + this.FirstName + " " + this.LastName);
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("Goodbye");
+            Console.ReadLine();
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Use overloaded operator
+            Employee employee1 = new Employee() { Id = 1 };
+            Employee employee2 = new Employee() { Id = 2 };
+
+            Console.WriteLine(employee1 == employee2);
+            Console.ReadLine();
+
+            //Employee sample = new Employee() { FirstName = "Sample", LastName = "Student" };
+
+            //sample.SayName();
+
+            //Console.ReadLine();
+            //sample.Quit();
+
+            //IQuittable employee = new Employee();
+            //employee.Quit();
+        }
+    }
+}
+
