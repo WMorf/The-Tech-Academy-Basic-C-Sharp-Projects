5c0ea3e5e9b1a5cb0d11f3687f4439b6defdb70c
Wesley Morford <wesmorford@outlook.com>
2022-10-25 16:16:32 -0700

    add DateTIme

---
+++ DateTIme/DateTIme/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTIme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Print the current date and time to the console.
+            Console.WriteLine(DateTime.Now);
+
+            //Ask the user for a number.
+            Console.WriteLine("Input a whole number");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            //Print to the console the exact time it will be in "input" hours
+            DateTime thisIsNow = DateTime.Now;
+            DateTime fromNow = thisIsNow.AddHours(input);
+            Console.WriteLine("The time {0} hours from now is " + fromNow, input);
+
+            Console.Read();
+        }
+    }
+}
--- TwentyOne/TwentyOne/Dealer.cs
+++ TwentyOne/TwentyOne/Dealer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace TwentyOne
 {
@@ -15,7 +16,14 @@ namespace TwentyOne
         public void Deal(List<Card> Hand)
         {
             Hand.Add(Deck.Cards.First());
-            Console.WriteLine(Deck.Cards.First().ToString() + "\n");
+            string card = string.Format(Deck.Cards.First().ToString() + "\n");
+            Console.WriteLine(card);
+            //automatically disposes of used resources when closing brace fires
+            using (StreamWriter file = new StreamWriter(@"C:\Users\techa\OneDrive\Desktop\Github\The-Tech-Academy-Basic-C-Sharp-Projects\TwentyOne\logs.txt", true))
+            {
+                file.WriteLine(DateTime.Now);
+                file.WriteLine(card);
+            }
             Deck.Cards.RemoveAt(0);
 
         }
